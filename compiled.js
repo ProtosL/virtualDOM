f44e145b9f4be0af7d93cad053218575db665201
ProtosL <644074553@qq.com>
2018-12-04 16:20:04 +0800

    add createElement function

--- compiled.js
+++ compiled.js
@@ -3,19 +3,29 @@ function h(type, props, ...children) {
     return { type, props, children };
 }
 
+function createElement(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+    const $el = document.createElement(node.type);
+    node.children.map(createElement).forEach($el.appendChild.bind($el));
+    return $el;
+}
+
 const a = h(
-    "ul",
-    { className: "list" },
+    'ul',
+    { className: 'list' },
     h(
-        "li",
+        'li',
         null,
-        "item 1"
+        'item 1'
     ),
     h(
-        "li",
+        'li',
         null,
-        "item 2"
+        'item 2'
     )
 );
 
-console.log(a);
+const $root = document.getElementById('root');
+$root.appendChild(createElement(a));
--- index.js
+++ index.js
@@ -3,11 +3,21 @@ function h (type, props, ...children) {
     return { type, props, children }
 }
 
+function createElement (node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node)
+    }
+    const $el = document.createElement(node.type)
+    node.children.map(createElement).forEach($el.appendChild.bind($el))
+    return $el
+}
+
 const a = (
     <ul className="list">
         <li>item 1</li>
         <li>item 2</li>
     </ul>
-);
+)
 
-console.log(a)
+const $root = document.getElementById('root')
+$root.appendChild(createElement(a))
